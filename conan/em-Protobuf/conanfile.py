6768b25cff414ce922f5e2aaf09ff4b13d4d76de
Konstantin Itskov <konstantin.itskov@kovits.com>
2018-04-01 21:51:51 -0400

    Upgrade em-protobuf to version 3.5.2 and make patches one git file

--- conan/em-Protobuf/conanfile.py
+++ conan/em-Protobuf/conanfile.py
@@ -1,34 +1,32 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
 from conans.tools import os_info, SystemPackageTool
-import shutil
 
 
 class ProtobufConan(ConanFile):
     name = "em-Protobuf"
-    version = "3.5.1"
+    version = "3.5.2"
     folder = "protobuf"
     url = "https://github.com/google/protobuf"
     description = "Protocol Buffers - Google's data interchange format"
     license = "https://github.com/google/protobuf/blob/master/LICENSE"
-    requires = "Protobuf/3.5.1@syncaide/stable", "emsdk/1.37.35@syncaide/stable"
+    requires = "Protobuf/3.5.2@syncaide/stable", "emsdk/1.37.35@syncaide/stable"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=False"
-    exports_sources = ["atomicops.h", "atomicops_internals_emscripten.h", "common.cc", "platform_macros.h", "port.h", "Makefile.am"]
+    exports_sources = ["emscripten.patch"]
     generators = "cmake"
+    packages = [
+        "autoconf",
+        "automake",
+        "libtool",
+        "curl",
+        "make",
+        "g++",
+        "unzip"
+    ]
 
     def source(self):
         self.run("git clone https://github.com/google/protobuf.git")
         self.run("git checkout tags/v{}".format(self.version), cwd=self.folder)
-
-        # Emscripten specific changes were made inside of these files.
-        # Followed the changes in: https://github.com/invokr/protobuf-emscripten
-        shutil.copy("atomicops.h", "{}/{}/src/google/protobuf/stubs/atomicops.h".format(self.source_folder, self.folder))
-        shutil.copy("atomicops_internals_emscripten.h", "{}/{}/src/google/protobuf/stubs/atomicops_internals_emscripten.h".format(self.source_folder, self.folder))
-        shutil.copy("common.cc", "{}/{}/src/google/protobuf/stubs/common.cc".format(self.source_folder, self.folder))
-        shutil.copy("platform_macros.h", "{}/{}/src/google/protobuf/stubs/platform_macros.h".format(self.source_folder, self.folder))
-        shutil.copy("port.h", "{}/{}/src/google/protobuf/stubs/port.h".format(self.source_folder, self.folder))
-        shutil.copy("Makefile.am", "{}/{}/src/Makefile.am".format(self.source_folder, self.folder))
+        self.run("git apply ../emscripten.patch", cwd=self.folder)
 
     def configure(self):
         self.options["Protobuf"].bin_only = True
@@ -36,7 +34,7 @@ class ProtobufConan(ConanFile):
     def system_requirements(self):
         if os_info.is_linux:
             installer = SystemPackageTool()
-            for pkg in ["autoconf", "automake", "libtool", "curl", "make", "g++", "unzip"]:
+            for pkg in self.packages:
                 installer.install(pkg)
 
     def build(self):
@@ -46,24 +44,14 @@ class ProtobufConan(ConanFile):
         with tools.environment_append(env.vars):
             self.run("./autogen.sh", cwd=self.folder)
 
-            args = ['--disable-dependency-tracking', '--with-protoc={}'.format(protoc)]
-            if not self.options.shared:
-                args += ['--disable-shared']
-            if self.options.shared or self.options.fPIC:
-                args += ['"CFLAGS=-fPIC" "CXXFLAGS=-fPIC"']
-
+            args = ['--disable-shared', '--with-protoc={}'.format(protoc)]
             self.run("emconfigure ./configure {}".format(' '.join(args)), cwd=self.folder)
             self.run("emmake make -j {}".format(tools.cpu_count()), cwd=self.folder)
 
     def package(self):
         self.copy("*.h", "include", "{}/src".format(self.folder))
         self.copy("*.proto", "include", "{}/src".format(self.folder))
-
-        self.copy("protoc", "bin", "{}/src/".format(self.folder), keep_path=False)
-        if not self.options.shared:
-            self.copy("*.a", "lib", "{}/src/.libs".format(self.folder), keep_path=False)
-        else:
-            self.copy("*.so*", "lib", "{}/src/.libs".format(self.folder), keep_path=False, symlinks=True)
+        self.copy("*.a", "lib", "{}/src/.libs".format(self.folder), keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["protobuf"]
+        self.cpp_info.libs = ["protobuf", "protobuf-lite"]
